94d2888a3de6c8ff3fbdaff82adf116e066b6d6e
Asif <152059881+Asifedit@users.noreply.github.com>
2024-09-08 14:16:24 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -10,7 +10,7 @@ const userRoutes = require("./Routes/main.routes");
 
 app.use(
     cors({
-        origin:"*",
+        origin:"https://clouddata-net.onrender.com",
         credentials: true,
     })
 );
